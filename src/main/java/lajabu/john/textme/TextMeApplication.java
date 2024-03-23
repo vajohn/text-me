c61f9c029690daa57e762fee303d04a04549246d
lajabujohn <jonny4jay@gmail.com>
2024-03-23 08:34:39 +0200

    Initialize

---
+++ src/main/java/lajabu/john/textme/TextMeApplication.java
@@ -0,0 +1,13 @@
+package lajabu.john.textme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TextMeApplication.class, args);
+  }
+
+}
